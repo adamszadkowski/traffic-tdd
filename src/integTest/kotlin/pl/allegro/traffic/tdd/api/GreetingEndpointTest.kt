aeda17b4dce5dda61ffce466b5ffd4b2cdf850ec
Adam Szadkowski <adam.szadkowski@allegro.pl>
2023-05-19 13:48:14 +0200

    Update greeting message

--- src/integTest/kotlin/pl/allegro/traffic/tdd/api/GreetingEndpointTest.kt
+++ src/integTest/kotlin/pl/allegro/traffic/tdd/api/GreetingEndpointTest.kt
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.put
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,6 +20,22 @@ class GreetingEndpointTest(
         mockMvc.get("/greeting").andExpect {
             status { is2xxSuccessful() }
             content { json(""" { "message": "hello world" } """) }
+        }
+    }
+
+    @Test
+    fun `update greeting`() {
+        mockMvc.put("/greeting") {
+            contentType = MediaType.APPLICATION_JSON
+            content = """ { "message": "updated greeting" } """
+        }.andExpect {
+            status { is2xxSuccessful() }
+            content { json(""" { "message": "updated greeting" } """) }
+        }
+
+        mockMvc.get("/greeting").andExpect {
+            status { is2xxSuccessful() }
+            content { json(""" { "message": "updated greeting" } """) }
         }
     }
 }
--- src/main/kotlin/pl/allegro/traffic/tdd/api/GreetingEndpoint.kt
+++ src/main/kotlin/pl/allegro/traffic/tdd/api/GreetingEndpoint.kt
@@ -1,17 +1,30 @@
 package pl.allegro.traffic.tdd.api
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.atomic.AtomicReference
 
 @RestController
 @RequestMapping("/greeting")
 class GreetingEndpoint {
+    private val message = AtomicReference("hello world")
 
     @GetMapping
-    fun getGreeting() = GreetingResponse("hello world")
+    fun getGreeting() =
+        GreetingResponse(message.get())
+
+    @PutMapping
+    fun updateGreeting(@RequestBody request: UpdateRequest) =
+        UpdateResponse(message.updateAndGet { request.message })
 }
 
 data class GreetingResponse(
+    val message: String,
+)
+
+data class UpdateRequest(
+    val message: String,
+)
+
+data class UpdateResponse(
     val message: String,
 )
